33e638f354cde8f7678910acc586f696ae8f6737
Maximillian Eugene <106041742+ray-caster@users.noreply.github.com>
2025-10-07 12:52:28 +0700

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.depanku.id';
 
 export interface AIMessage {
     role: 'user' | 'assistant';
